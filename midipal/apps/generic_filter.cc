0422f50fb6bbcac90e35c3d15e8fb6599190555b
pichenettes <ol.gillet@gmail.com>
2011-11-08 02:14:33 +0100

    Added general purpose filter app

---
+++ midipal/apps/generic_filter.cc
@@ -0,0 +1,229 @@
+// Copyright 2011 Olivier Gillet.
+//
+// Author: Olivier Gillet (ol.gillet@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+//
+// Generic filter app.
+
+#include <avr/eeprom.h>
+
+#include "midipal/apps/generic_filter.h"
+
+#include "avrlib/random.h"
+#include "avrlib/op.h"
+
+#include "midi/midi.h"
+
+#include "midipal/display.h"
+#include "midipal/ui.h"
+
+namespace midipal { namespace apps {
+
+using namespace avrlib;
+
+const prog_uint8_t generic_filter_factory_data[1] PROGMEM = { 0 };
+
+/* static */
+Modifier GenericFilter::modifiers_[kNumModifiers];
+
+/* static */
+uint8_t GenericFilter::active_program_;
+
+/* static */
+const prog_AppInfo GenericFilter::app_info_ PROGMEM = {
+  &OnInit, // void (*OnInit)();
+  NULL, // void (*OnNoteOn)(uint8_t, uint8_t, uint8_t);
+  NULL, // void (*OnNoteOff)(uint8_t, uint8_t, uint8_t);
+  NULL, // void (*OnNoteAftertouch)(uint8_t, uint8_t, uint8_t);
+  NULL, // void (*OnAftertouch)(uint8_t, uint8_t);
+  NULL, // void (*OnControlChange)(uint8_t, uint8_t, uint8_t);
+  NULL, // void (*OnProgramChange)(uint8_t, uint8_t);
+  NULL, // void (*OnPitchBend)(uint8_t, uint16_t);
+  NULL, // void (*OnAllSoundOff)(uint8_t);
+  NULL, // void (*OnResetAllControllers)(uint8_t);
+  NULL, // void (*OnLocalControl)(uint8_t, uint8_t);
+  NULL, // void (*OnAllNotesOff)(uint8_t);
+  NULL, // void (*OnOmniModeOff)(uint8_t);
+  NULL, // void (*OnOmniModeOn)(uint8_t);
+  NULL, // void (*OnMonoModeOn)(uint8_t, uint8_t);
+  NULL, // void (*OnPolyModeOn)(uint8_t);
+  NULL, // void (*OnSysExStart)();
+  NULL, // void (*OnSysExByte)(uint8_t);
+  NULL, // void (*OnSysExEnd)();
+  NULL, // void (*OnClock)();
+  NULL, // void (*OnStart)();
+  NULL, // void (*OnContinue)();
+  NULL, // void (*OnStop)();
+  NULL, // void (*OnActiveSensing)();
+  NULL, // void (*OnReset)();
+  NULL, // uint8_t (*CheckChannel)(uint8_t);
+  NULL, // void (*OnRawByte)(uint8_t);
+  &OnRawMidiData, // void (*OnRawMidiData)(uint8_t, uint8_t*, uint8_t, uint8_t);
+  NULL, // void (*OnInternalClockTick)();
+  NULL, // void (*OnInternalClockStep)();
+  NULL, // uint8_t (*OnIncrement)(int8_t);
+  NULL, // uint8_t (*OnClick)();
+  NULL, // uint8_t (*OnPot)(uint8_t, uint8_t);
+  NULL, // uint8_t (*OnRedraw)();
+  NULL, // void (*OnIdle)();
+  NULL, // void (*SetParameter)(uint8_t, uint8_t);
+  NULL, // uint8_t (*GetParameter)(uint8_t);
+  NULL, // uint8_t (*CheckPageStatus)(uint8_t);
+  1, // settings_size
+  SETTINGS_GENERIC_FILTER_PROGRAM, // settings_offset
+  &active_program_, // settings_data
+  generic_filter_factory_data, // factory_data
+  STR_RES_MAPPER, // app_name
+};  
+
+/* static */
+void GenericFilter::OnInit() {
+  ui.AddPage(STR_RES_PRG, UNIT_INDEX, 0, 3);
+  memset(modifiers_, 0, sizeof(modifiers_));
+  /*modifiers_[0].channel_bitmask = 1;
+  modifiers_[0].message_type_bitmask = 0x03;
+  modifiers_[0].value[0].min = 0;
+  modifiers_[0].value[0].max = 127;
+  modifiers_[0].value[1].min = 0;
+  modifiers_[0].value[1].max = 127;
+  modifiers_[0].action = 0x13;
+  modifiers_[0].value_transformation[0].operation = 0;
+  modifiers_[0].value_transformation[0].argument[0] = 12;
+  modifiers_[0].value_transformation[0].argument[1] = 0;
+  modifiers_[0].value_transformation[1].operation = 5 | 0x80;
+  modifiers_[0].value_transformation[1].argument[0] = 1;
+  modifiers_[0].value_transformation[1].argument[1] = 127;
+  
+  modifiers_[1].channel_bitmask = 1;
+  modifiers_[1].message_type_bitmask = 0x40;
+  modifiers_[1].value[0].min = 0;
+  modifiers_[1].value[0].max = 127;
+  modifiers_[1].value[1].min = 0;
+  modifiers_[1].value[1].max = 127;
+  modifiers_[1].action = 0x00;
+  modifiers_[1].value_transformation[0].operation = 3;
+  modifiers_[1].value_transformation[0].argument[0] = 0;
+  modifiers_[1].value_transformation[0].argument[1] = 0;
+  modifiers_[1].value_transformation[1].operation = 6;
+  modifiers_[1].value_transformation[1].argument[0] = -12;
+  modifiers_[1].value_transformation[1].argument[1] = 12;*/
+}
+
+/* static */
+void GenericFilter::SetParameter(uint8_t key, uint8_t value) {
+  active_program_ = value;
+  uint16_t address = value * sizeof(modifiers_);
+  address += SETTINGS_GENERIC_FILTER_SETTINGS;
+  eeprom_read_block(
+      &modifiers_[0],
+      (void*)(address),
+      sizeof(modifiers_));
+}
+
+/* static */
+void GenericFilter::OnRawMidiData(
+   uint8_t status,
+   uint8_t* data,
+   uint8_t data_size,
+   uint8_t accepted_channel) {
+  uint8_t destroyed = 0;
+  uint8_t forwarded = 0;
+  for (uint8_t i = 0; i < kNumModifiers; ++i) {
+    const Modifier& modifier = modifiers_[i];
+    if (modifier.accept(status, data[0], data[1])) {
+      destroyed = destroyed || modifier.destroys();
+      forwarded = forwarded || modifier.forwards();
+    }
+  }
+  
+  if (destroyed) {
+    return;
+  }
+  
+  if (forwarded) {
+    app.Send(status, data, data_size);
+  }
+  
+  // The rules are now applied one by one.
+  for (uint8_t i = 0; i < kNumModifiers; ++i) {
+    const Modifier& modifier = modifiers_[i];
+    if (modifier.accept(status, data[0], data[1])) {
+      // Apply a transformation on the channel.
+      uint8_t channel = status;
+      if (modifier.remaps_channel()) {
+        channel = modifier.action;
+      } else {
+        channel += modifier.action;
+      }
+      status = (status & 0xf0) | (channel & 0x0f);
+      
+      // Apply a transformation on the values
+      for (uint8_t j = 0; j < 2; ++j) {
+        ValueTransformation t = modifier.value_transformation[j];
+        int16_t value = data[j];
+        switch (t.operation & 0x0f) {
+          case VALUE_OEPRATION_ADD:
+            value += t.argument[0];
+            break;
+
+          case VALUE_OPERATION_SUBTRACT:
+            value -= t.argument[0];
+            break;
+
+          case VALUE_OPERATION_INVERT:
+            value = 127 - value;
+            break;
+
+          case VALUE_OPERATION_SET_TO:
+            value = t.argument[0];
+            break;
+          
+          case VALUE_OPERATION_SET_TO_RANDOM:
+            value = Random::GetByte() & 0x7f;
+            // Fall through
+
+          case VALUE_OPERATION_MAP_TO_RANGE:
+            if (t.argument[0] < t.argument[1]) {
+              value = U8U8MulShift8(value << 1, t.argument[1] - t.argument[0]);
+              value += t.argument[0];
+            } else {
+              value = U8U8MulShift8(value << 1, t.argument[0] - t.argument[1]);
+              value = t.argument[1] - value;
+            }
+            break;
+
+          case VALUE_OPERATION_ADD_RANDOM:
+            value += U8U8MulShift8(
+                Random::GetByte(),
+                t.argument[1] - t.argument[0]);
+            value += t.argument[0];
+            break;
+        }
+        if (t.preserves_zero() && data[j] == 0) {
+          value = 0;
+        }
+        if (t.wrap()) {
+          data[j] = value & 0x7f;
+        } else {
+          data[j] = Clip(value, 0x00, 0x7f);
+        }
+      }
+    }
+  }
+  
+  app.Send(status, data, data_size);
+}
+
+} }  // namespace midipal::apps
---
+++ midipal/apps/generic_filter.h
@@ -0,0 +1,130 @@
+// Copyright 2011 Olivier Gillet.
+//
+// Author: Olivier Gillet (ol.gillet@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+//
+// Generic filter app.
+
+#ifndef MIDIPAL_APPS_GENERIC_FILTER_H_
+#define MIDIPAL_APPS_GENERIC_FILTER_H_
+
+#include "midipal/app.h"
+
+namespace midipal { namespace apps {
+
+static const uint8_t kNumModifiers = 4;
+
+enum ValueOperation {
+  VALUE_OEPRATION_ADD,
+  VALUE_OPERATION_SUBTRACT,
+  VALUE_OPERATION_INVERT,
+  VALUE_OPERATION_SET_TO,
+  VALUE_OPERATION_MAP_TO_RANGE,
+  VALUE_OPERATION_SET_TO_RANDOM,
+  VALUE_OPERATION_ADD_RANDOM,
+};
+
+struct Range {
+  int8_t min;
+  int8_t max;
+};
+
+struct ValueTransformation {
+  uint8_t operation;
+  int8_t argument[2];
+  
+  inline uint8_t preserves_zero() const {
+    return operation & 0x80;
+  }
+  
+  inline uint8_t wrap() const {
+    return operation & 0x40;
+  }
+};
+
+struct Modifier {
+  // Event filtering.
+  uint16_t channel_bitmask;
+  uint8_t message_type_bitmask;
+  Range value[2];
+
+  // Action / Transformation
+  uint8_t action;
+  uint16_t delay;
+  ValueTransformation value_transformation[2];
+  
+  inline uint8_t accept_channel(uint8_t channel) const {
+    uint16_t channel_bit = 1;
+    channel_bit <<= channel;
+    return channel_bit & channel_bitmask;
+  }
+  
+  inline uint8_t accept_message(uint8_t message) const {
+    uint8_t message_bit = 1 << ((message >> 4) - 8);
+    return message_bit & message_type_bitmask;
+  }
+  
+  inline uint8_t accept(uint8_t status, uint8_t data1, uint8_t data2) const {
+    if ((status & 0xf0) == 0xf0) {
+      // For realtime messages, only test the message type.
+      return accept_message(status);
+    } else {
+      return accept_channel(status & 0x0f) && \
+          accept_message(status) && \
+          data1 >= value[0].min && \
+          data1 <= value[0].max && \
+          data2 >= value[1].min && \
+          data2 <= value[1].max;
+    }
+  }
+  
+  inline uint8_t destroys() const {
+    return action & 0x80;
+  }
+  
+  inline uint8_t forwards() const {
+    return action & 0x40;
+  }
+  
+  inline uint8_t remaps_channel() const {
+    return action & 0x10;
+  }
+};
+
+class GenericFilter {
+ public:
+  GenericFilter() { }
+
+  static void OnInit();
+  static void OnRawMidiData(
+     uint8_t status,
+     uint8_t* data,
+     uint8_t data_size,
+     uint8_t accepted_channel);
+ 
+  static void SetParameter(uint8_t key, uint8_t value);
+ 
+  static const prog_AppInfo app_info_;
+ 
+ private:
+  static uint8_t active_program_;
+  static Modifier modifiers_[kNumModifiers];
+  
+  DISALLOW_COPY_AND_ASSIGN(GenericFilter);
+};
+
+} }  // namespace midipal::apps
+
+#endif // MIDIPAL_APPS_GENERIC_FILTER_H_
